5db09718a4075a75383a54041cedd8e9fb83e6d1
a1865304 <yangqingyan0@gmail.com>
2023-08-17 12:58:36 +0930

    need to update c++ version

---
+++ src/input/main.cpp
@@ -0,0 +1,10 @@
+#include "readInput.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Reading input file..." << std::endl;
+    readInput();
+    std::cout << "Finished reading input file." << std::endl;
+    return 0;
+}
--- src/input/readInput.cpp
+++ src/input/readInput.cpp
@@ -1,9 +1,29 @@
 #include "readInput.h"
 #include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 void readInput()
 {
     // Read input file using the file name stored in GlobalVars::inputFileName
     // and store the data in GlobalVars::processTasks
-    // testing
+
+    GlobalVars::inputFileName = "./input.txt";
+
+    std::ifstream file(GlobalVars::inputFileName);
+
+    if (!file)
+    {
+        std::cerr << "Unable to open file: " << GlobalVars::inputFileName << std::endl;
+        return;                                                         
+    }
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::cout << line << std::endl;
+    }
+
+    file.close();
 }
