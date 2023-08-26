ce910fb92faaf7ae8805ee14864542078b01e586
WanxiaJaneYang <1016887513@qq.com>
2023-08-26 18:54:23 +0930

    change main accordingly

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,6 @@ int main()
     GlobalVars::inputFileName = "input.txt";
     GlobalVars::outputFileName = "output.txt";
 
-    MultiThreadsHandler::startThreads();
+    startThreads();
     return 0;
 }
