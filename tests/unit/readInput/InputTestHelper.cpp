c43cdab45fe2c5a0866324182ebbbb138744e038
WanxiaJaneYang <1016887513@qq.com>
2023-08-27 10:24:42 +0930

    fix::passed the maptek test, delete all the read input and output part from the main

--- tests/unit/readInput/InputTestHelper.cpp
+++ tests/unit/readInput/InputTestHelper.cpp
@@ -18,7 +18,7 @@ std::vector<std::string> listInputFiles(const std::string& directory) {
 std::string writeContentOfTasks(SafeInputTasks& tasks) {
     std::ostringstream oss;
     for (int i = 0; i < tasks.size(); ++i) {
-        Block& block = tasks.task(i);
+        Block& block = tasks.tasks[i];
         // Assuming Block has a method that returns a string representation
         oss << "\n\n" << "Block" << (i + 1) << writeContentOfBlock(block);
     }
