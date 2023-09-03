f202a83f0dd129aa38466546c654cbf312eb92ea
a1866621 <a1866621@adelaide.edu.au>
2023-09-03 16:13:55 +0930

    fix: sorted result in unit test for comparasion

--- tests/integration/integrationValidTests.cpp
+++ tests/integration/integrationValidTests.cpp
@@ -9,38 +9,40 @@ TEST_P(IntegrationTest, HandlesValidIntegration) {
     // Get the expected file path according to the name convention
     std::string testFilePath = GetParam();
 
-    std::cerr << "Find open file path: " << std::endl;
-
     // Read the content of the file into an istringstream
     std::ifstream inFile(testFilePath);
 
+    // simulate input stream
     std::stringstream buffer;
     buffer << inFile.rdbuf();
-
     std::istringstream simulatedInput(buffer.str());
     std::cerr << "simulated input "<< std::endl;
 
-    // startThread()
     readInput(simulatedInput); 
     std::cerr << "input finished" << std::endl;
-
     Compressor::compress(); 
-
     std::cerr << "compression finished" << std::endl;
 
     // Redirect std::cout to a stringstream
     std::ostringstream oss;
     std::streambuf* pbuf = std::cout.rdbuf();
     std::cout.rdbuf(oss.rdbuf());
-
     // Call the output function
     output();
     std::cerr << "output finished" << std::endl;
 
     // Restore std::cout
     std::cout.rdbuf(pbuf);
+    
+    // Split the output into lines and sort them
     std::istringstream iss(oss.str());
-    std::string producedContent = iss.str();
+    std::vector<std::string> ProducedLines;
+    std::string line;
+    while (std::getline(iss, line)) {
+        line.erase(std::remove(line.begin(), line.end(), '\r'), line.end());
+        ProducedLines.push_back(line);
+    }
+    std::sort(ProducedLines.begin(), ProducedLines.end());  
 
     // find the expected result
     std::string baseName = std::__fs::filesystem::path(testFilePath).stem().string();
@@ -50,12 +52,19 @@ TEST_P(IntegrationTest, HandlesValidIntegration) {
     }
     std::string expectedFilePath = "../../tests/integration/valid_test_cases/" + baseName + "_expected.txt";
 
-    // read the expected content into a string
+    // Convert expectedContent into a vector of lines
     std::ifstream expectedFile(expectedFilePath);
     std::string expectedContent((std::istreambuf_iterator<char>(expectedFile)), std::istreambuf_iterator<char>());
+    std::istringstream expectedStream(expectedContent);
+    std::vector<std::string> expectedLines;
+    while (std::getline(expectedStream, line)) {
+        line.erase(std::remove(line.begin(), line.end(), '\r'), line.end());
+        expectedLines.push_back(line);
+    }
+    std::sort(expectedLines.begin(), expectedLines.end());
 
     // compare the two strings to see if they are the same
-    ASSERT_EQ(expectedContent, producedContent) << "The expected and produced contents are not the same!";
+    ASSERT_EQ(expectedLines, ProducedLines) << "The expected and produced contents are not the same!";
 }
 
 // Instantiate the test case with the files in the directory
