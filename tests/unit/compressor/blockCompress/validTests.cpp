314cc3dbef897c000aa6e242b78c0dc9918579be
a1865304 <yangqingyan0@gmail.com>
2023-09-08 01:02:22 +0930

    feat(unit test/compressor/block/validtest): write compare logic

--- tests/unit/compressor/blockCompress/validTests.cpp
+++ tests/unit/compressor/blockCompress/validTests.cpp
@@ -1,11 +1,13 @@
-#include "InputTestHelper.h"
+#include "blockCompressTestHelper.h"
 
 // Parameterized test class
-class ReadInputTest : public ::testing::TestWithParam<std::string> {};
+class BlockCompressTest : public ::testing::TestWithParam<std::string>
+{
+};
 
 // Actual test definition
-TEST_P(ReadInputTest, HandlesValidInput) {
-
+TEST_P(BlockCompressTest, HandlesValidInput)
+{
     // Get the expected file path according to the name convention
     std::string testFilePath = GetParam();
 
@@ -13,19 +15,25 @@ TEST_P(ReadInputTest, HandlesValidInput) {
     std::ifstream inFile(testFilePath);
     std::stringstream buffer;
     buffer << inFile.rdbuf();
-    std::istringstream simulatedInput(buffer.str());
+    std::string fileContent = buffer.str();
 
-    // read the readInput() produced content into a string
-    readInput(simulatedInput); // change to use std::istringstream as input type
-    std::string producedContent = writeReadContent();
+    // Convert file content to the required data structure
+    //std::deque<std::deque<Cuboid>> planes = convertFileContentToPlanes(fileContent);
+
+    // Read the blockCompress() produced content into a string
+    //blockCompress(planes);
+
+    std::string producedContent = writeReadContent(); // method to generate output to string
 
     // find the expected result
     std::string baseName = std::__fs::filesystem::path(testFilePath).stem().string();
-        size_t pos = baseName.rfind("_case");
-    if (pos != std::string::npos) {
-        baseName.erase(pos, 5); // 5 is the length of "_case"
+    size_t pos = baseName.rfind("_case");
+    if (pos != std::string::npos)
+    {
+        baseName.erase(pos, 6); // 5 is the length of "_case"
     }
-    std::string expectedFilePath = "../../tests/unit/readInput/valid_test_cases/" + baseName + "_expected.txt";
+
+    std::string expectedFilePath = "../../tests/unit/compressor/blockCompress/valid_test_cases/" + baseName + "_expected.txt";
 
     // read the expected content into a string
     std::ifstream expectedFile(expectedFilePath);
@@ -38,6 +46,5 @@ TEST_P(ReadInputTest, HandlesValidInput) {
 // Instantiate the test case with the files in the directory
 INSTANTIATE_TEST_SUITE_P(
     FileBasedTests,
-    ReadInputTest,
-    ::testing::ValuesIn(listInputFiles("../../tests/unit/readInput/valid_test_cases/"))
-);
+    BlockCompressTest,
+    ::testing::ValuesIn(listInputFiles("../../tests/unit/compressor/blockCompress/valid_test_cases/")));
