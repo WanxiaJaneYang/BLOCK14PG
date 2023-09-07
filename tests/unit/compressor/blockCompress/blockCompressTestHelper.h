c5a9c10f622a55ff554f378bedbd493c3dda9cfa
a1865304 <yangqingyan0@gmail.com>
2023-09-08 01:16:56 +0930

    fix: add std::string writeContentOfCuboid(Cuboid &cuboid) to helper header

--- tests/unit/compressor/blockCompress/blockCompressTestHelper.h
+++ tests/unit/compressor/blockCompress/blockCompressTestHelper.h
@@ -15,6 +15,7 @@
 std::vector<std::string> listInputFiles(const std::string &directory);
 std::deque<std::deque<Cuboid>> convertFileContentToPlanes(const std::string &fileContent);
 std::string writeContentOfTasks(SafeInputTasks &tasks);
+std::string writeContentOfCuboid(Cuboid &cuboid);
 std::string writeReadContent();
 void clearTasks(SafeInputTasks &tasks);
 
