97e90b6a0dadddd0fddc59978edbbb7fe3f9e431
a1865304 <yangqingyan0@gmail.com>
2023-09-08 01:05:41 +0930

    feat: create listInputFiles method to get all input files from a directory based on prev code

--- tests/unit/compressor/blockCompress/blockCompressTestHelper.cpp
+++ tests/unit/compressor/blockCompress/blockCompressTestHelper.cpp
@@ -1,12 +1,13 @@
 #include "blockCompressTestHelper.h"
 
 // Utility function to get all input files from a directory
-std::vector<std::string> listInputFiles(const std::string& directory) {
+std::vector<std::string> listInputFiles(const std::string &directory)
+{
     std::vector<std::string> files;
-    for (const auto& entry : std::__fs::filesystem::directory_iterator(directory)) {
-        // if (entry.path().extension() == ".txt" && entry.path().string().find("_case.txt") != std::string::npos) {
-
-        if (entry.path().extension() == ".csv" && entry.path().string().find("_case.csv") != std::string::npos) {
+    for (const auto &entry : std::__fs::filesystem::directory_iterator(directory))
+    {
+        if (entry.path().extension() == ".txt" && entry.path().string().find("_case.txt") != std::string::npos)
+        {
             std::cout << entry.path().string() << std::endl;
             files.push_back(entry.path().string());
         }
