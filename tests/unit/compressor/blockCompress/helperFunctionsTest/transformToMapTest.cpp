78bdbf0fba0b4b73f312dbfa433c547225accdb4
WanxiaJaneYang <1016887513@qq.com>
2023-09-13 20:32:44 +0930

    upload one more test cases

--- tests/unit/compressor/blockCompress/helperFunctionsTest/transformToMapTest.cpp
+++ tests/unit/compressor/blockCompress/helperFunctionsTest/transformToMapTest.cpp
@@ -11,10 +11,9 @@
 TEST(TransformToMapTest, BasicTest)
 {
     // Arrange
-    std::deque<Cuboid> plane = {
-        Cuboid(0, 0, 0, 0, 0, 0, 1, 1, 1, 'A'),
-        Cuboid(0, 0, 0, 0, 1, 0, 2, 2, 1, 'B'),
-    };
+    std::deque<Cuboid> plane;
+    plane.push_back(Cuboid(0, 0, 0, 0, 0, 0, 1, 1, 1, 'A'));
+    plane.push_back(Cuboid(0, 0, 0, 0, 1, 0, 2, 2, 1, 'B'));
 
     std::map<CuboidKey, Cuboid> expectedCuboids;
     expectedCuboids[CuboidKey{Point(0, 0), Point(0, 0)}] = Cuboid(0, 0, 0, 0, 0, 0, 1, 1, 1, 'A');
@@ -34,6 +33,27 @@ TEST(TransformToMapTest, EmptyInput)
     // Arrange
     std::deque<Cuboid> plane = {};
     std::map<CuboidKey, Cuboid> expectedCuboids = {};
+    std::map<CuboidKey, Cuboid> actualCuboids;
+
+    // Act
+    transformToMap(plane, actualCuboids);
+
+    // Assert
+    EXPECT_TRUE(areMapsEqual(expectedCuboids, actualCuboids));
+}
+
+TEST(TransformToMapTest, MoreCuboids)
+{
+    // Arrange
+    std::deque<Cuboid> plane;
+    plane.push_back(Cuboid(0, 0, 0, 0, 0, 0, 3, 4, 1, 'A'));
+    plane.push_back(Cuboid(0, 0, 0, 3, 0, 0, 4, 4, 1, 'B'));
+    plane.push_back(Cuboid(0, 0, 0, 0, 4, 0, 5, 2, 1, 'C'));
+
+    std::map<CuboidKey, Cuboid> expectedCuboids;
+    expectedCuboids[CuboidKey{Point(0, 0), Point(2, 3)}] = Cuboid(0, 0, 0, 0, 0, 0, 3, 4, 1, 'A');
+    expectedCuboids[CuboidKey{Point(3, 0), Point(6, 3)}] = Cuboid(0, 0, 0, 3, 0, 0, 4, 4, 1, 'B');
+    expectedCuboids[CuboidKey{Point(0, 4), Point(4, 5)}] = Cuboid(0, 0, 0, 0, 4, 0, 5, 2, 1, 'C');
     std::map<CuboidKey, Cuboid> actualCuboids;
 
     // Act
