64fc90000f8b69911588587d7eb6648289089bb4
Kaini Chang personal <100549067+KainiChang@users.noreply.github.com>
2023-10-08 17:48:45 +1030

    feat: Finish MultiThreadsHandler implementation

--- src/multi-threads-handling/MultiThreadsHandler.cpp
+++ src/multi-threads-handling/MultiThreadsHandler.cpp
@@ -4,22 +4,41 @@
 #include "../compressor/Compressor.h"
 #include <thread>
 
+const int MAX_THREADS = 8;
+
 void startThreads()
 {
     // just a stub, waiting for the implementation of the threads
-    readInput();
-    Compressor::compress();
-    output();
+    startReadingThread();
+
+    std::vector<std::thread> threads;
+    for (int i = 0; i < MAX_THREADS; ++i)
+    {
+        threads.emplace_back(startCompressingThread);
+    }
+
+    for (auto &t : threads)
+    {
+        if (t.joinable())
+        {
+            t.join();
+        }
+    }
+    // output();
 }
 
 static void startReadingThread()
 {
-}
-
-static void startWritingThread()
-{
+    std::thread readerThread(readInput);
+    readerThread.detach();
 }
 
 static void startCompressingThread()
 {
+    Compressor::compress();
+    output();
 }
+
+// static void startWritingThread()
+// {
+// }
