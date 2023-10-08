189a5eff6355a93dddf8b9c9463586c9f396827e
Kaini Chang personal <100549067+KainiChang@users.noreply.github.com>
2023-10-08 18:15:42 +1030

    feat: Update MultiThreadsHandler.cpp on output thread

--- src/multi-threads-handling/MultiThreadsHandler.cpp
+++ src/multi-threads-handling/MultiThreadsHandler.cpp
@@ -3,42 +3,30 @@
 #include "../output/output.h"
 #include "../compressor/Compressor.h"
 #include <thread>
-
-const int MAX_THREADS = 8;
+#include "ThreadPool.h" 
 
 void startThreads()
 {
-    // just a stub, waiting for the implementation of the threads
-    startReadingThread();
-
-    std::vector<std::thread> threads;
-    for (int i = 0; i < MAX_THREADS; ++i)
-    {
-        threads.emplace_back(startCompressingThread);
-    }
-
-    for (auto &t : threads)
-    {
-        if (t.joinable())
+    // Start dedicated threads for input and output
+    std::thread inputThread(readInput);
+    std::thread outputThread(output); // I need a function never ends
+    
+    inputThread.detach();
+    outputThread.detach();
+    
+    ThreadPool pool(6);
+    
+    pool.enqueue(readInput);
+    pool.enqueue(output);
+    pool.enqueue(startWritingThread)
+    
+      for (int i = 0; i < 6; ++i)
         {
-            t.join();
+            pool.enqueue(Compressor::compress);
         }
-    }
-    // output();
+    
+    inputThread.join();
+    outputThread.join();
+    
+    // ThreadPool's destructor will wait for all tasks to complete before the main function exits
 }
-
-static void startReadingThread()
-{
-    std::thread readerThread(readInput);
-    readerThread.detach();
-}
-
-static void startCompressingThread()
-{
-    Compressor::compress();
-    output();
-}
-
-// static void startWritingThread()
-// {
-// }
