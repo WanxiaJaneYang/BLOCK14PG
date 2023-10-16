19e8c983435dbe5136b1ae112a874c6e19bcec46
WanxiaJaneYang <1016887513@qq.com>
2023-10-16 22:50:37 +1030

    fix(changed header): d

--- src/compressor/Compressor.h
+++ src/compressor/Compressor.h
@@ -9,4 +9,5 @@
 void compress();
 std::deque<std::deque<std::deque<Cuboid>>> lineCompress(Block &block);
 std::deque<std::deque<Cuboid>> planeCompress(std::deque<std::deque<std::deque<Cuboid>>> &compressedLines);
+void blockCompress(std::deque<std::deque<Cuboid>> &planes);
 #endif
